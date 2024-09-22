1c8762a1d5a66ca0c4b75d0c5a3a413593699129
imagobea <himagobea@gmail.com>
2024-09-22 18:26:02 +0200

    feat: add a data class to the spring boot project

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -3,13 +3,18 @@ package com.example.demo
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+
+data class Message(val id: String?, val text: String)
 
 @RestController
 class MessageController {
     @GetMapping("/")
-    fun index(@RequestParam("name") name: String) = "Hello, $name!"
+    fun index() = listOf(
+        Message("1", "hola"),
+        Message("2", "hello"),
+        Message("3", "konnichiwa")
+    )
 }
 
 @SpringBootApplication
