b6635a28acf76349e8024029cbab940ed628b8c4
imagobea <himagobea@gmail.com>
2024-09-22 19:02:21 +0200

    feat: add database support, create message service

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -2,19 +2,40 @@ package com.example.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import java.util.*
 
 data class Message(val id: String?, val text: String)
 
+@Service
+class MessageService(val db: JdbcTemplate) {
+    fun findMessages(): List<Message> = db.query("select * from messages") { response, _ ->
+        Message(response.getString("id"), response.getString("text"))
+    }
+
+    fun save(message: Message) {
+        val id = message.id ?: UUID.randomUUID().toString()
+        db.update(
+            "insert into messages values (?, ?)",
+            id, message.text
+        )
+    }
+}
+
 @RestController
-class MessageController {
+class MessageController(val service: MessageService) {
     @GetMapping("/")
-    fun index() = listOf(
-        Message("1", "hola"),
-        Message("2", "hello"),
-        Message("3", "konnichiwa")
-    )
+    fun index(): List<Message> = service.findMessages()
+
+    @PostMapping("/")
+    fun post(@RequestBody message: Message) {
+        service.save(message)
+    }
 }
 
 @SpringBootApplication
