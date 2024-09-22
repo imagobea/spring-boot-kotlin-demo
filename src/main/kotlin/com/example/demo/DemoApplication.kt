65b320eef5d83050445b7ac8abac0cd55e59b084
imagobea <himagobea@gmail.com>
2024-09-22 19:17:54 +0200

    feat: add endpoint to retrieve messages by id

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -3,11 +3,9 @@ package com.example.demo
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.jdbc.core.query
 import org.springframework.stereotype.Service
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import java.util.*
 
 data class Message(val id: String?, val text: String)
@@ -15,6 +13,10 @@ data class Message(val id: String?, val text: String)
 @Service
 class MessageService(val db: JdbcTemplate) {
     fun findMessages(): List<Message> = db.query("select * from messages") { response, _ ->
+        Message(response.getString("id"), response.getString("text"))
+    }
+
+    fun findMessageById(id: String): List<Message> = db.query("select * from messages where id = ?", id) { response, _ ->
         Message(response.getString("id"), response.getString("text"))
     }
 
@@ -31,6 +33,10 @@ class MessageService(val db: JdbcTemplate) {
 class MessageController(val service: MessageService) {
     @GetMapping("/")
     fun index(): List<Message> = service.findMessages()
+
+    @GetMapping("/{id}")
+    fun index(@PathVariable id: String): List<Message> =
+        service.findMessageById(id)
 
     @PostMapping("/")
     fun post(@RequestBody message: Message) {
